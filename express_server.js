264af1be75f976d7115029e372dfdc7e248da360
serra <serra@modemgirl.ca>
2022-03-03 14:27:25 -0800

    refix links in show template

--- express_server.js
+++ express_server.js
@@ -49,14 +49,15 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { 
     username: req.cookies['username'],
-    urls: urlDatabase 
+    urls: urlDatabase,
+    shortURL: urlDatabase
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
   const templateVars = { username: req.cookies['username']};
-  res.render("urls_new");
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -68,7 +69,7 @@ app.post("/urls", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { 
     shortURL: req.params.shortURL, 
-    longURL: urlDatabase, 
+    longURL: urlDatabase[req.params.shortURL], 
     username: req.cookies['username']
   };
   res.render("urls_show", templateVars);
